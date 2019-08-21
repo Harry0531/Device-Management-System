a5724367d4a1e4a9fa817b63f2c6a0f29edb908c
zhu-ch <43909561+zhu-ch@users.noreply.github.com>
2019-08-21 21:32:19 +0800

    security product - done

---
+++ src/main/webapp/static/js/securityProduct/scrappedSecurityProducts.js
@@ -0,0 +1,172 @@
+let defaultFiltersCondition = {
+    use_situation: '',
+    scope: '',
+    secret_level: '',
+    type: '',
+    school: '',
+    subject: '',
+    startTime: '',
+    endTime: ''
+};
+
+let defaultDialog = {
+    visible: false,
+    loading: false,
+    data: {
+        id: '',
+        remarks: '',
+        scrap_time: ''
+    }
+};
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        urls: {
+            getSub: 'http://localhost:8444/api/sys/product/scrapped/getSub',
+            getDeptSub: 'http://localhost:8444/api/sys/product/scrapped/getDeptSub',
+            getList: 'http://localhost:8444/api/sys/product/scrapped/getList',
+            scrap: 'http://localhost:8444/api/sys/product/scrapped/scrap'
+        },
+        loading: false,
+        activeNames: [],
+        filters: {
+            selectionList: {
+                use_situation: [],
+                scope: [],
+                secret_level: [],
+                type: [],
+                school: [],
+                subject: []
+            },
+            condition: defaultFiltersCondition
+        },
+        table: {
+            loading: false,
+            selectionList: [],
+            data: [],
+            props: {
+                searchKey: '',
+                pageIndex: 1,
+                pageSize: 10,
+                pageSizes: [5, 10, 20, 40],
+                total: 0
+            }
+        },
+        dialog: defaultDialog
+    },
+    methods: {
+        getSub() {
+            ajaxPost(this.urls.getSub, {param: "使用范围"}, function (result) {
+                app.filters.selectionList.scope.push({'value': '', 'label': '全选'});
+                result.forEach(function (r) {
+                    app.filters.selectionList.scope.push({'value': r.id, 'label': r.dicValue});
+                });
+            });
+            ajaxPost(this.urls.getSub, {param: "类型"}, function (result) {
+                app.filters.selectionList.type.push({'value': '', 'label': '全选'});
+                result.forEach(function (r) {
+                    app.filters.selectionList.type.push({'value': r.id, 'label': r.dicValue});
+                });
+            });
+            ajaxPost(this.urls.getSub, {param: "密级"}, function (result) {
+                app.filters.selectionList.secret_level.push({'value': '', 'label': '全选'});
+                result.forEach(function (r) {
+                    app.filters.selectionList.secret_level.push({'value': r.id, 'label': r.dicValue});
+                });
+            });
+            ajaxPost(this.urls.getSub, {param: "使用情况"}, function (result) {
+                app.filters.selectionList.use_situation.push({'value': '', 'label': '全选'});
+                result.forEach(function (r) {
+                    app.filters.selectionList.use_situation.push({'value': r.id, 'label': r.dicValue});
+                });
+            });
+            ajaxPost(this.urls.getSub, {param: "dept"}, function (result) {
+                result.forEach(function (r) {
+                    app.filters.selectionList.school.push(r);
+                })
+            })
+        },
+        getDeptSub: function (index) {
+            app.filters.selectionList.subject = [];
+            app.filters.subject = '';
+            ajaxPost(this.urls.getDeptSub, {id: index}, function (result) {
+                result.forEach(function (r) {
+                    app.filters.selectionList.subject.push(r);
+                });
+            })
+        },
+        refreshTable: function () {
+            let app = this;
+            app.table.loading = true;
+            let data = {
+                page: app.table.props,
+                scope: this.filters.condition.scope,
+                type: this.filters.condition.type,
+                secret_level: this.filters.condition.secret_level,
+                use_situation: this.filters.condition.use_situation,
+                department_code: this.filters.condition.school,
+                subject_code: this.filters.condition.subject,
+                startTime: this.filters.condition.startTime,
+                endTime: this.filters.condition.endTime
+            };
+            ajaxPostJSON(this.urls.getList, data, function (result) {
+                app.table.loading = false;
+                app.table.data = result.data.resultList;
+                app.table.props.total = result.data.total;
+            })
+        },
+        scrap: function () {
+            app.dialog.loading = true;
+            let data = {
+                id: app.dialog.data.id,
+                remarks: app.dialog.data.remarks,
+                scrap_time: app.dialog.data.scrap_time
+            };
+            ajaxPostJSON(this.urls.scrap, data, function (d) {
+                app.dialog.loading = false;
+                app.dialog.visible = false;
+                app.$message({
+                    message: "操作成功",
+                    type: "success"
+                });
+                app.getList();
+            })
+        },
+        getList: function (index) {
+            app.table.props.pageIndex = 1;
+            this.refreshTable();
+        },
+        showDialog: function (v) {
+            let app = this;
+            app.dialog.data.id = v["id"];
+            app.dialog.data.remarks = v["remarks"];
+            app.dialog.data.scrap_time = v["scrap_time"];
+            app.dialog.visible = true;
+        },
+        onSelectionChange: function (val) {
+            this.table.selectionList = val;
+        },
+        onPageSizeChange: function (newSize) {
+            this.table.props.pageSize = newSize;
+            this.refreshTable();
+        },
+        onPageIndexChange: function (newIndex) {
+            this.table.props.pageIndex = newIndex;
+            this.refreshTable();
+        }
+    },
+    mounted: function () {
+        this.getSub();
+        this.refreshTable();
+    },
+    computed: {
+        visitDate: function () {
+            let nowDate = new Date();
+            let year = nowDate.getFullYear();
+            let month = nowDate.getMonth() + 1;
+            let day = nowDate.getDate();
+            return year + "-" + month + "-" + day;
+        }
+    }
+});
